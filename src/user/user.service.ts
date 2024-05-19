fcc0df89150a3503b7839181029a54b0b6815290
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-05-19 05:42:08 -0300

    add createUser method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async createUser(
+    name: string,
+    lastName: string,
+    adress: string,
+    profilePicture: string,
+    password: string,
+  ) {
+    const newUser = new this.userModel({
+      name,
+      lastName,
+      adress,
+      profilePicture,
+      password,
+    });
+    const result = await newUser.save();
+    return result.id as string;
+  }
+}
