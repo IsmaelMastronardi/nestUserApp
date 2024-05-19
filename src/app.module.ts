0226d000ae68d7d30017e83e8eab5622437cb1b0
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-05-19 05:26:44 -0300

    add DatabaseModule UserModule and MongooseModule imports to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user/user.model';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [
+    DatabaseModule,
+    UserModule,
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
