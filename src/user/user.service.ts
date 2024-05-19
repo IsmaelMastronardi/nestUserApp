057db5e9d9df95d199bec32b4bccb413ab5e7775
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-05-19 05:54:10 -0300

    add updateUser method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.model';
@@ -37,5 +37,46 @@ export class UserService {
         profilePicture: user.profilePicture,
       })),
     };
+  }
+
+  async updateUser(
+    id: string,
+    name: string,
+    lastName: string,
+    adress: string,
+    profilePicture: string,
+    password: string,
+  ) {
+    const updateUser = await this.findUser(id);
+
+    if (name) {
+      updateUser.name = name;
+    }
+    if (lastName) {
+      updateUser.lastName = lastName;
+    }
+    if (adress) {
+      updateUser.address = adress;
+    }
+    if (profilePicture) {
+      updateUser.profilePicture = profilePicture;
+    }
+    if (password) {
+      updateUser.password = password;
+    }
+    updateUser.save();
+  }
+
+  private async findUser(id: string): Promise<User> {
+    let user;
+    try {
+      user = await this.userModel.findById(id);
+    } catch (error) {
+      throw new NotFoundException('Could not find user');
+    }
+    if (!user) {
+      throw new NotFoundException('Could not find user');
+    }
+    return user;
   }
 }
