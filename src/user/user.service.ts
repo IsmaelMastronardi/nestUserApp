769defcb73599f3da041e2f64f26b6366e8e1d2f
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-05-19 17:31:48 -0300

    update @Post route to utilize multer to allow a file

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,23 +3,19 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.model';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
 
   async createUser(
-    name: string,
-    lastName: string,
-    address: string,
+    createUserDto: CreateUserDto,
     profilePicture: string,
-    password: string,
-  ) {
-    const hashedPassword = await bcrypt.hash(password, 10);
+  ): Promise<string> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
     const newUser = new this.userModel({
-      name,
-      lastName,
-      address,
+      ...createUserDto,
       profilePicture,
       password: hashedPassword,
     });
