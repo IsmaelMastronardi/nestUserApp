ca5dbe8c8c2630846f1f2e57001dfd25f290f680
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-05-19 05:43:04 -0300

    add createUser method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(
+    @Body('name') userName: string,
+    @Body('lastName') userLastName: string,
+    @Body('adress') userAdress: string,
+    @Body('profilePicture') userProfilePicture: string,
+    @Body('password') userPassword: string,
+  ) {
+    const generatedId = await this.userService.createUser(
+      userName,
+      userLastName,
+      userAdress,
+      userProfilePicture,
+      userPassword,
+    );
+    return { id: generatedId };
+  }
+}
