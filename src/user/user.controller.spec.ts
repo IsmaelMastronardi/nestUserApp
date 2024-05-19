da5341a7ae7ee6e7331cb51ab6c4bf4bcc71447e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-05-19 18:03:41 -0300

    solve bug with importing UserModules and DatabaseConnection dependencies

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserModule } from './user.module';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-    }).compile();
+      imports: [UserModule],
+    })
+      .overrideProvider(getModelToken('User'))
+      .useValue(jest.fn())
+      .compile();
 
     controller = module.get<UserController>(UserController);
   });
