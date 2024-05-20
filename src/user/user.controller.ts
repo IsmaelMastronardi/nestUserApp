d41a688119742866dacedbd0d6918fe78a9fdfb5
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-05-20 20:28:17 -0300

    add swagger decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -14,13 +14,40 @@ import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { multerOptions } from '../common/multer.config';
 import { CreateUserDto } from './dto/create-user.dto';
+import {
+  ApiBody,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('user')
+@ApiTags('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(AuthGuard('basic'))
+  @Get()
+  @ApiQuery({ name: 'address', required: true, description: 'User address' })
+  @ApiQuery({ name: 'password', required: true, description: 'User password' })
+  @ApiResponse({
+    status: 200,
+    description: 'Will return the number of users, and the detail for each one',
+  })
+  async getAllUsers() {
+    const users = await this.userService.getUsers();
+    return users;
+  }
+
   @Post()
   @UseInterceptors(FileInterceptor('profilePicture', multerOptions))
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Will create a user with the given parameters,the profilePicture paramenter can be absent, otherwise, it must be a file',
+  })
   async createUser(
     @Body() createUserDto: CreateUserDto,
     @UploadedFile() file: Express.Multer.File,
@@ -30,15 +57,16 @@ export class UserController {
   }
 
   @UseGuards(AuthGuard('basic'))
-  @Get()
-  async getAllUsers() {
-    const users = await this.userService.getUsers();
-    return users;
-  }
-
-  @UseGuards(AuthGuard('basic'))
   @Patch(':id')
   @UseInterceptors(FileInterceptor('profilePicture', multerOptions))
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiQuery({ name: 'address', required: true, description: 'User address' })
+  @ApiQuery({ name: 'password', required: true, description: 'User password' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Will update the user with the given id, any parameter can be absent',
+  })
   async updateUser(
     @Param('id') userId: string,
     @Body('name') userName: string,
