09d1351f7d11b32d3861229ff9b3b44880c71377
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-05-20 17:05:51 -0300

    change database.module to use environment variables

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/node-nest-test')],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
 })
 export class DatabaseModule {}
